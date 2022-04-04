00f80e38aa688a391ec9f5591a1b91c309c05e18
MatteoKastler <matteo.kastler@gmail.com>
2022-04-04 11:46:16 +0200

    fixed mqtt import

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
-import mqtt as mqtt
+import paho.mqtt.client as mqtt
 
 counter = 0
 def on_message(client, userdata, message):
     if(message.topic == 'notify'):
         counter+1
+        print("new picture taken")
 
 client = mqtt.Client('countClient')
 client.connect('127.0.0.1') #ip von mosquitto broker
